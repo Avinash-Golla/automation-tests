a576c0be1d6c800162375f3a2c56b99fe36cb860
Avinash_Golla <avinashgolla2505@gmail.com>
2025-07-09 07:54:36 +0530

    First commit 09/07

---
+++ src/test/java/com/yourorg/LoginTest.java
@@ -0,0 +1,11 @@
+package com.yourorg;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    @Test
+    public void loginWithSpecialChars() {
+        System.out.println("Running: loginWithSpecialChars");
+    }
+}
---
+++ src/test/java/com/yourorg/PaymentTest.java
@@ -0,0 +1,12 @@
+package com.yourorg;
+
+import org.testng.annotations.Test;
+
+public class PaymentTest {
+
+
+    @Test
+    public void paymentWithSpecialChars() {
+        System.out.println("Running: paymentWithSpecialChars");
+    }
+}
