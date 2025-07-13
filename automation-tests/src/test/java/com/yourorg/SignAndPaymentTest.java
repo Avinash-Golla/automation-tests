f8810788f9b1cfb9b2d8837ab3bcf18ea014161e
Avinash Golla <137623743+Avinash-Golla@users.noreply.github.com>
2025-07-13 19:34:15 +0530

    Add files via upload

---
+++ automation-tests/src/test/java/com/yourorg/SignAndPaymentTest.java
@@ -0,0 +1,12 @@
+package com.yourorg;
+
+import org.testng.annotations.Test;
+
+public class SignAndPaymentTest {
+
+
+    @Test
+    public void SignAndPaymentIssue() {
+        System.out.println("Running: sign and payment not working properly");
+    }
+}
---
+++ automation-tests/src/test/java/com/yourorg/SignTest.java
@@ -0,0 +1,12 @@
+package com.yourorg;
+
+import org.testng.annotations.Test;
+
+public class SignTest {
+
+    @Test
+    public void SignIssue()
+    {
+        System.out.println("This is sign Issue");
+    }
+}
